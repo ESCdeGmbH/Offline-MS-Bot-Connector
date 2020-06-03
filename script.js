f21ceb5e0d1ff652db1e4ae1583d7984734ffdb0
Christoph Wieland <wieland@escde.net>
2020-06-03 16:32:43 +0200

    use require.js to load adaptivecards.js and signalr.js via script.js, moved connectionbuilder to script.js, send messages via input field, updated button-actions of adaptive cards

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+var ac;
+var sr;
+
 let chatfieldTypes = Object.freeze({ "user": 1, "bot": 2 });
 let conversationId;
 let config;
@@ -8,8 +11,12 @@ function InitBot(cfg, botSection) {
     config = cfg;
     conversationId = uuid();
     console.log("Conversation ID: " + conversationId);
-    SetUpBotView(botSection);
-    SetUpSignalR();
+    require(["https://unpkg.com/adaptivecards/dist/adaptivecards.js", "https://cdnjs.cloudflare.com/ajax/libs/microsoft-signalr/3.1.3/signalr.min.js"], function (module_ac, module_sr) {
+        ac = module_ac;
+        sr = module_sr;
+        SetUpBotView(botSection);
+        SetUpSignalR();
+    });
 }
 
 function SetUpBotView(botSection) {
@@ -42,31 +49,32 @@ function SetUpBotView(botSection) {
 }
 
 function SetUpSignalR() {
-    let connection = config.receiver_hub;
+    let connection = new sr.HubConnectionBuilder()
+        .withUrl(config.receiver_hub)
+        .configureLogging(sr.LogLevel.Information)
+        .build();
 
     connection.onclose(async () => {
         await start(connection);
     });
 
     connection.on(conversationId, async () => {
-        const resp = await fetch(config.reply_to + conversationId + "/poll", {
+        const resp = await fetch(config.reply_to + "v3/conversations/" + conversationId + "/poll", {
             method: 'POST'
         });
         const status = await resp.status;
         const result = await resp.json();
         if (status != 200 || !result || result.length == 0) {
-            Console.log("Error while parsing the response.");
+            console.error("Error while parsing the response.");
             return;
         }
-        switch (result[0].type) {
-            case "message":
-                HandleMessage(result[0].text);
-                break;
-            case "asasdasd":
-                HandleAdaptiveCard(result[0].attachments);
-                break;
-            default: HandleUnknownType();
+        if (result[0].type != "message") {
+            HandleUnknownType();
+            return;
         }
+
+        if (result[0].text.length > 0) HandleMessage(result[0].text);
+        HandleAdaptiveCard(result[0].attachments);
     });
 
     start(connection);
@@ -77,14 +85,16 @@ async function start(connection) {
         await connection.start();
         console.log("connected");
     } catch (err) {
-        console.log(err);
+        console.error(err);
         setTimeout(() => start(), 5000);
     }
 };
 
 function HandleAdaptiveCard(attachments) {
-    for (var at in attachments) {
-        CreateAndAppendChatField(CreateAdaptiveCard(at.content), chatfieldTypes.bot);
+    if (attachments) {
+        for (let i = 0; i < attachments.length; i++) {
+            CreateAndAppendChatField(CreateAdaptiveCard(attachments[i].content), chatfieldTypes.bot);
+        }
     }
 }
 
@@ -105,10 +115,31 @@ function SubmitInput(event) {
     document.getElementById("inputField").value = "";
     CreateAndAppendChatField(CreateText(input), chatfieldTypes.user);
     event.preventDefault();  // to prevent the page to getting redirected after clicking the submit button or pressing 'Enter'
+    SendMessageToBot(input);
 }
 
-function SendMessageToBot(text) {
-    // TODO
+async function SendMessageToBot(text) {
+    let payload = {
+        "channelId": "ESC_Offline_Connector",
+        "conversation": {
+            "id": conversationId
+        },
+        "from": {
+            "id": config.user_id
+        },
+        "id": uuid(),
+        "serviceUrl": config.reply_to,
+        "text": text,
+        "type": "message"
+    }
+    const resp = await fetch(config.message_backend, {
+        method: 'POST',
+        body: JSON.stringify(payload)
+    });
+    const msgStatus = await resp.status;
+    if (msgStatus != 200) {
+        console.error("Sending message failed with status code: " + msgStatus);
+    }
 }
 
 function CreateAndAppendChatField(renderedContent, type) {
@@ -157,14 +188,15 @@ function CreateText(content) {
     return paragraph;
 }
 
-function CreateAdaptveCard(content) {
-    let adaptiveCard = new AdaptiveCards.AdaptiveCard();
-    adaptiveCard.hostconfig = new AdaptiveCards.Hostconfig({
-        fontFamily: document.body.fontFamily
-        // More host config options
-    });
+function SubmitACInput(text) {
+    CreateAndAppendChatField(CreateText(text), chatfieldTypes.user);
+    SendMessageToBot(text);
+}
+
+function CreateAdaptiveCard(content) {
+    let adaptiveCard = new ac.AdaptiveCard();
     adaptiveCard.onExecuteAction = function (action) {
-        let chatField = CreateChatField(action.text, chatfieldTypes.user); // TODO Variable Text prüfen
+        let chatField = SubmitACInput(action.title);
         document.getElementById("chat-container").appendChild(chatField);
     }
     adaptiveCard.parse(content);
